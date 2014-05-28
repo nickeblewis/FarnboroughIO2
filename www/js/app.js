1178de8a7100d8e38c27cf73892c33d568688b3c
nickeblewis@gmail.com <nickeblewis@gmail.com>
2014-05-28 13:40:14 +0000

    Experimenting with sorting of feed list

--- www/js/app.js
+++ www/js/app.js
@@ -19,6 +19,25 @@ angular.module('fg', ['firebase', 'ionic', 'fg.controllers', 'fg.services', 'fg.
     }
   });
 })
+.filter('reverse', function() {
+      function toArray(list) {
+         var k, out = [];
+         if( list ) {
+            if( angular.isArray(list) ) {
+               out = list;
+            }
+            else if( typeof(list) === 'object' ) {
+               for (k in list) {
+                  if (list.hasOwnProperty(k)) { out.push(list[k]); }
+               }
+            }
+         }
+         return out;
+      }
+      return function(items) {
+         return toArray(items).slice().reverse();
+      };
+   })
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
